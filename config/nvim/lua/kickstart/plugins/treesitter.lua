90c69f885ede878eecf7dc81651e5ab1d110cba0
Matthew Rowe <mnrowe@protonmail.com>
2026-02-09 17:25:02 -0500

    update

--- config/nvim/lua/kickstart/plugins/treesitter.lua
+++ config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -1,12 +1,14 @@
 return {
   { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
     config = function()
-      local filetypes = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" }
-      require("nvim-treesitter").install(filetypes)
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = filetypes,
-        callback = function() vim.treesitter.start() end,
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" },
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
       })
     end,
   },
